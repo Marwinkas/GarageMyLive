6d4b90e51595b4da27c3b6a275c4d29404031041
Marwinkas <marwinkas@mail.ru>
2025-03-05 19:29:11 +0300

    Старт

---
+++ Source/GarageMyLive/GarageMyLiveGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GarageMyLiveGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGarageMyLiveGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGarageMyLiveGameMode();
+};
+
+
+
